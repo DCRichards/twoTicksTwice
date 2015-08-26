3a1d727e6f7022dcd0e9d297f78177add6b41267
DCRichards <DCRichards@live.co.uk>
2015-08-26 21:10:15 +0100

    invert UI colours and add battery img for status

--- src/main.c
+++ src/main.c
@@ -14,8 +14,12 @@ static TextLayer *s_date_layer;
 static TextLayer *s_battery_layer;
 
 static BitmapLayer *s_bt_bitmap_layer;
+static BitmapLayer *s_batt_bitmap_layer;
+
 static GBitmap *s_bt_connected_bitmap;
 static GBitmap *s_bt_disconnected_bitmap;
+static GBitmap *s_batt_bitmap;
+static GBitmap *s_batt_chrg_bitmap;
 
 static GFont ds_digital_font_60;
 
@@ -46,13 +50,14 @@ static void update_battery() {
     static char s_battery_buffer[16];
     BatteryChargeState charge_state = battery_state_service_peek();
     
-    if (charge_state.is_charging) {
-        snprintf(s_battery_buffer, sizeof(s_battery_buffer), "CHRG");    
+    if (charge_state.is_charging) { 
+        text_layer_set_text(s_battery_layer, NULL);
+        bitmap_layer_set_bitmap(s_batt_bitmap_layer, s_batt_chrg_bitmap);
     } else {
-        snprintf(s_battery_buffer, sizeof(s_battery_buffer), "%02d%%", charge_state.charge_percent);
+        bitmap_layer_set_bitmap(s_batt_bitmap_layer, s_batt_bitmap);
+        snprintf(s_battery_buffer, sizeof(s_battery_buffer), "%02d", charge_state.charge_percent);
+        text_layer_set_text(s_battery_layer, s_battery_buffer);
     }
-    
-    text_layer_set_text(s_battery_layer, s_battery_buffer);
 }
 
 static void update_bt_status() {
@@ -77,16 +82,15 @@ static void bt_handler(bool connected) {
 }
 
 static void main_window_load(Window *window) {
-    
     // bluetooth layer
     s_bt_connected_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BT_IMG_CON);
     s_bt_disconnected_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BT_IMG_DISCON);
-    s_bt_bitmap_layer = bitmap_layer_create(GRect(100, 10, 28, 28));
+    s_bt_bitmap_layer = bitmap_layer_create(GRect(110, 10, 28, 28));
     
     // time layer
     s_time_layer = text_layer_create(GRect(0, 40, 144, 80));
     text_layer_set_background_color(s_time_layer, GColorClear);
-    text_layer_set_text_color(s_time_layer, GColorBlack);
+    text_layer_set_text_color(s_time_layer, GColorWhite);
     text_layer_set_text(s_time_layer, "00:00");
     ds_digital_font_60 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_DS_DIGITAL_60));
     text_layer_set_font(s_time_layer, ds_digital_font_60);
@@ -95,21 +99,28 @@ static void main_window_load(Window *window) {
     // date layer
     s_date_layer = text_layer_create(GRect(0, 120, 144, 50));
     text_layer_set_background_color(s_date_layer, GColorClear);
-    text_layer_set_text_color(s_date_layer, GColorBlack);
+    text_layer_set_text_color(s_date_layer, GColorWhite);
     text_layer_set_text(s_date_layer, "31 Sept");
     text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
     text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
     
     // battery layer
-    s_battery_layer = text_layer_create(GRect(15, 10, 50, 20));
+    s_batt_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BATT_IMG);
+    s_batt_chrg_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BATT_IMG_CHRG);
+    s_batt_bitmap_layer = bitmap_layer_create(GRect(10, 8, 65, 34));
+    
+    s_battery_layer = text_layer_create(GRect(15, 9, 50, 50));
     text_layer_set_background_color(s_battery_layer, GColorClear);
-    text_layer_set_text(s_battery_layer, "100%%");
-    text_layer_set_font(s_battery_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+    text_layer_set_text_color(s_battery_layer, GColorWhite);
+    text_layer_set_text(s_battery_layer, "100");
+    text_layer_set_font(s_battery_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
     text_layer_set_text_alignment(s_battery_layer, GTextAlignmentCenter);
     
+    // assign layers - order is important here
     layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bt_bitmap_layer));
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+    layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_batt_bitmap_layer));
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_battery_layer));
 }
 
@@ -120,6 +131,9 @@ static void main_window_unload(Window *window) {
     
     gbitmap_destroy(s_bt_connected_bitmap);
     gbitmap_destroy(s_bt_disconnected_bitmap);
+    gbitmap_destroy(s_batt_bitmap);
+    gbitmap_destroy(s_batt_chrg_bitmap);
+    bitmap_layer_destroy(s_batt_bitmap_layer);
     bitmap_layer_destroy(s_bt_bitmap_layer);
     
     fonts_unload_custom_font(ds_digital_font_60);
@@ -137,6 +151,7 @@ static void init() {
         .unload = main_window_unload
     });
     
+    window_set_background_color(s_main_window, GColorBlack);
     window_stack_push(s_main_window, true);
     
     // we need to update at the start
